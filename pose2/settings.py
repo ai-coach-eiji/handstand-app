e5913fb0f160d9373084f396266dad28877052bc
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-16 00:07:31 +0900

    modified settings file

--- pose2/settings.py
+++ pose2/settings.py
@@ -126,12 +126,12 @@ WSGI_APPLICATION = 'pose2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': env('DATABASE_NAME'), 
+        'USER': env('DATABASE_USER'), 
+        'PASSWORD': env('DATABASE_PASSWORD'), 
+        'HOST': env('DATABASE_HOST'), 
+        'PORT': '5432'
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
