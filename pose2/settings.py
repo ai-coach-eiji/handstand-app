91d561e7c0b70a59d48a1eac7149fcb2d6b77526
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-16 12:46:22 +0900

    modified env reference

--- pose2/settings.py
+++ pose2/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'pose2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2', 
-        'NAME':env('DATABASE_NAME'), 
+        'NAME':"env('DATABASE_NAME')", 
         'USER':env('DATABASE_USER'), 
         'PASSWORD':env('DATABASE_PASSWORD'), 
         'HOST':env('DATABASE_HOST'), 
