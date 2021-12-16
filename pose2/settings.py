8cb01d64236b33c5dc76a8b360f81f9441171267
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-16 13:34:53 +0900

    modified DATABASE default

--- pose2/settings.py
+++ pose2/settings.py
@@ -133,6 +133,7 @@ WSGI_APPLICATION = 'pose2.wsgi.application'
         'PORT':'5432'
     }
 }'''
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
