ae15407eff05796e486eac61fd336eb02657b122
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-16 18:25:18 +0900

    deleted  STATICFILES_DIRS path

--- pose2/settings.py
+++ pose2/settings.py
@@ -174,11 +174,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = 'staticfiles'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
